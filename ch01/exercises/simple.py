881082a98bc73d11251249c33831298fc4f5c3e7
Joshua Janason <112440235+JoshuaJanason@users.noreply.github.com>
2022-09-06 22:05:20 +0000

    Working with data exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,17 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15) #The last answer is a repeating decimal and does not end
+rate = input("Enter current exchange rate for the Euro to Dollar: ")
+amount = input("Enter the amount of currency you would like to exchange: ")
+rate = float(rate)
+amount = float(amount)
+total = rate * amount
+result = total - 3.00
+print(result)
