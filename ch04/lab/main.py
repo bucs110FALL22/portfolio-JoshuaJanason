2b085826d56e9ba490d7bd1c0d9cf9f97ef050d8
Joshua Janason <112440235+JoshuaJanason@users.noreply.github.com>
2022-10-08 01:27:08 +0000

    Dart Game Lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -2,29 +2,40 @@ import pygame
 import math
 import random
 
-pygame.init()
-surface = (900, 600)
-screen = pygame.display.set_mode(surface)
-pygame.display.init()
-length = surface[0]
-width = surface[1]
-print(length, width)
-
-#Square
-coords = []
-num_sides = 4
-side_length = 50
-offset = 100
+#Part A
+#Screen
+screen = pygame.display.set_mode((300, 300))
+width, height = pygame.display.get_window_size()
+print(width, height)
 color = "blue"
-x = 200
-y = 300
-l = 900
-w = 600
-coords.append([x, y, l, w])
-screen.fill("white")
-pygame.draw.rect(screen, "blue", [x, y, l, w])
+screen.fill(color)
+
+#Circle
+color = "yellow"
+pygame.draw.circle(screen, color, (150, 150), 150)
 pygame.display.flip()
-pygame.time.wait(1000)
+pygame.time.wait(500)
+
+#Lines
+color = "black"
+pygame.draw.line(screen, color, (0, 150), (300, 150))
+pygame.display.flip()
+pygame.time.wait(500)
+pygame.draw.line(screen, color, (150, 0), (150, 300))
+pygame.display.flip()
+pygame.time.wait(500)
+
+#Part B
+color = "green"
+random_width = random.randrange(0, 301)
+random_length = random.randrange(0, 301)
+for i in range(10):
+  x1 = random_width
+  y1 = random_length
+pygame.draw.circle(screen, color, (x1, y1), 5)
+pygame.display.flip()
+pygame.time.wait(500)
 
 
-  
+
+
