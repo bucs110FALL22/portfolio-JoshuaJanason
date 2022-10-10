72756595d9ddca51d212283c4568d026ab304fdf
Joshua Janason <112440235+JoshuaJanason@users.noreply.github.com>
2022-10-10 22:01:39 +0000

    Patterns.py

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -6,36 +6,79 @@ import random
 #Screen
 screen = pygame.display.set_mode((300, 300))
 width, height = pygame.display.get_window_size()
+gameStart = False
 print(width, height)
 color = "blue"
 screen.fill(color)
+
+#pygame.draw.rect(screen, "green",(50,150,50,50))
+#pygame.display.flip()
+#pygame.time.wait(1000)
+#pygame.draw.rect(screen, "red" ,(200,150,50,50))
+#pygame.display.flip()
+#pygame.time.wait(1000)
+
+#Part C
+#Choose a Player
+choose_player = input("Do you think player 1 or player 2 will win?")
+print(choose_player)
+
 
 #Circle
 color = "yellow"
 pygame.draw.circle(screen, color, (150, 150), 150)
 pygame.display.flip()
 pygame.time.wait(500)
-
+    
+  
 #Lines
 color = "black"
 pygame.draw.line(screen, color, (0, 150), (300, 150))
 pygame.display.flip()
-pygame.time.wait(500)
+pygame.time.wait(100)
 pygame.draw.line(screen, color, (150, 0), (150, 300))
 pygame.display.flip()
-pygame.time.wait(500)
+pygame.time.wait(100)
 
 #Part B
+#Player 1
+x2 = 150
+y2 = 150
 color = "green"
-random_width = random.randrange(0, 301)
-random_length = random.randrange(0, 301)
 for i in range(10):
+  random_width = random.randrange(0, 301)
+  random_length = random.randrange(0, 301)
   x1 = random_width
   y1 = random_length
-pygame.draw.circle(screen, color, (x1, y1), 5)
-pygame.display.flip()
-pygame.time.wait(500)
-
-
-
+  distance_from_center = math.hypot(x1-x2, y1-y2) 
+  is_in_circle = distance_from_center <= width/2
+  print(is_in_circle)
+  if is_in_circle is True: 
+    color = "green"
+  elif is_in_circle is False:
+    color = "orange"
+  pygame.draw.circle(screen, color, (x1, y1), 5)
+  pygame.display.flip()
+  print(distance_from_center)
+  print(is_in_circle)
+  pygame.time.wait(500)
+  
+#Player 2
+for i in range(10):
+  random_width = random.randrange(0, 301)
+  random_length = random.randrange(0, 301)
+  x1 = random_width
+  y1 = random_length
+  distance_from_center = math.hypot(x1-x2, y1-y2) 
+  is_in_circle = distance_from_center <= width/2
+  print(is_in_circle)
+  if is_in_circle is True: 
+    color = "red"
+  elif is_in_circle is False:
+    color = "purple"
+  pygame.draw.circle(screen, color, (x1, y1), 5)
+  pygame.display.flip()
+  print(distance_from_center)
+  print(is_in_circle)
+  pygame.time.wait(500)
 
---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,18 @@
+#Part 1
+def star_pyramid():
+    print("Enter how many lines of stars you would like")
+    rows = int(input(":"))
+    for i in range(1, rows + 1):      
+      print("*" * i)
+
+star_pyramid()
+
+
+#Part 2 
+def rstar_pyramid():
+    print("Enter how many lines of stars you would like")
+    r_rows = int(input(":"))
+    for i in range(1, r_rows + 1):      
+      print("*" * i)
+
+rstar_pyramid()
